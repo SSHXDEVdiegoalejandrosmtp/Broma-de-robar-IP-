a2dd2854717dac5f14d1eff62f343662a5c365d8
xzyAlejandroSMTP <sshxdevdiegoalejandrogo@gmail.com>
2024-05-22 00:20:25 +0100

    shell <-> stupid
    Use this code to open a file.
    Use these functions in the source code to access the database.
    This program code needs to be tidied up.

---
+++ you.sh
@@ -0,0 +1,30 @@
+${}
+echo  " your computer is tracked   ::                 false  "
+
+fullkey  =  ""
+read -p  "Enter acess data XZ :: "
+hak = ""
+read -p " Deleting  dont arrest me       " hak 
+
+read -p "Continue? (Y/N): " confirm && [[ confirm == [yY][eE][sS] ]] || exit 1
+
+cat >  ip.apodaca.mx.txt
+
+cat    ip.apodaca.mx.txt  < 192.04.030.214 "api-false" choco init {loop} "false" 
+windows 14 detect call center 
+windows 7  detect  aplee-air 
+android    undected  
+close _>
+echo " Gobierno Interceptando Red     MrRoot  --> sne to ******    "
+echo  "false   4"
+echo " false 0  "
+echo " false  ? "
+clear 
+echo "---------------------------> this is a false  broma for your friends            "
+echo "    </mail>     sshxdevdiegoalejandrogo@gmail.com"
+sleep(10)
+clear 
+
+exit()
+
+
